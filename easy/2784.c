69de0f5402bbd3756c9248d454a456c7060b7155
Seth Stephanz <sethstephanz@gmail.com>
2026-05-15 23:32:52 -0500

    Finish 2784

--- easy/2784.c
+++ easy/2784.c
@@ -6,27 +6,10 @@
 // 2784. Check if Array is Good
 // https://leetcode.com/problems/check-if-array-is-good/description/?envType=daily-question&envId=2026-05-14
 
-typedef struct {
-    char *data;
-    size_t size;
-    size_t cap;
-} Str;
-
-int print_int_array(int *arr, int arr_size) {
-    if (arr_size == 0) {
-        fprintf(stderr, "[]");
-    }
-    fprintf(stderr, "[");
-    for (int i = 0; i < arr_size - 1; i++) {
-        fprintf(stderr, "%d ", arr[i]);
-    }
-    fprintf(stderr, "%d]\n", arr[arr_size - 1]);
-    return 0;
-}
+// qsort, callback practice. otherwise just condition checking
 
 int comp(const void *a, const void *b) {
-    // comp is passed to qsort as a function pointer later
-    // but it needs this function signature, then just cast everything
+    // comp callback for qsort
     int x = *(const int *)a;
     int y = *(const int *)b;
 
@@ -43,28 +26,31 @@ bool isGood(int *nums, int numsSize) {
     // make sure sorted array includes all values 1-(n-1),
     // ensure the maximum value occurs exactly twice
 
-    // array has to start with 1 and last two elements have to be maximum of array
-    // so, [1, 1] is fine, but [9, 9] is invalid because does not include numbers up to 9
-    // basically, [1, 1] is only two-element array that works
-
+    // array too small
     if (numsSize < 2) {
-        // array too small
         return false;
     }
 
-    qsort(nums, numsSize, sizeof(nums[0]), comp);
+    qsort(nums, (size_t)numsSize, sizeof(nums[0]), comp); // sort array
 
+    // can't include full sequence
     if (nums[0] != 1) {
-        // can't include full sequence
         return false;
     }
 
-    for (int i = 1; i < numsSize - 2; i++) {
+    // [1, 1, 1], etc.
+    if (numsSize > 2 && nums[0] == 1 && nums[numsSize - 1] == 1) {
+        return false;
+    }
+
+    // check sequence
+    for (int i = 1; i < numsSize - 1; i++) {
         if (nums[i] != (nums[i - 1] + 1)) {
             return false;
         }
     }
 
+    // last and second-to-last elements don't match
     if (nums[numsSize - 2] != nums[numsSize - 1]) {
         return false;
     }
