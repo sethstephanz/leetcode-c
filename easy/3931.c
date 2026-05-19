30872a7cf5b2b4de4a0d83d7a972568591674f7f
Seth Stephanz <sethstephanz@gmail.com>
2026-05-19 09:48:35 -0500

    Add loop optimization to 3931

--- easy/3931.c
+++ easy/3931.c
@@ -6,11 +6,13 @@ https://leetcode.com/problems/check-adjacent-digit-differences/description/
 Return true if absolute value between any adjacent pair of digits in a digit string > 2. Else, false
 
 pattern: simulation, ~two-pointer
+
+optimization: if string is guaranteed to be valid, 
+use while not null terminator in loops instead of finding strlen directly to save O(n) operation 
 */
 
 bool isAdjacentDiffAtMostTwo(char* s) {
-    int s_len = strlen(s);
-    for (int i = 1; i < s_len; i++) {
+    for (int i = 1; s[i] != '\0'; i++) {
         if (abs(s[i-1] - s[i]) > 2) {
             return false;
         }
